7e1336a9055bf3b477298797902784ec5136c81e
Nathan Greene <nathangreene3@gmail.com>
2020-02-17 23:50:29 -0500

    Finished testing for sums

--- sum.go
+++ sum.go
@@ -1,77 +1,91 @@
 package scratch
 
-import "fmt"
+func sum0(values []int) int {
+	var (
+		s int
+		n = len(values)
+	)
 
-func toInt(x interface{}) int {
-	switch t := x.(type) {
-	case int:
-		return x.(int)
-	case *int:
-		return int(*x.(*int))
-	default:
-		panic(fmt.Sprintf("unsupported type %t", t))
-	}
-}
-
-func sum(values []int) int {
-	var s int
-	n := len(values)
 	for i := 0; i < n; i++ {
 		s += values[i]
 	}
-	return s
-}
 
-func sum0(values []int) int {
-	var s int
-	for i := range values {
-		s += values[i]
-	}
 	return s
 }
 
 func sum1(values []int) int {
 	var s int
-	for _, v := range values {
-		s += v
-	}
-	return s
-}
-
-func sum2(values ...int) int {
-	var s int
 	for i := range values {
 		s += values[i]
 	}
+
+	return s
+}
+
+func sum2(values []int) int {
+	var s int
+	for _, v := range values {
+		s += v
+	}
+
 	return s
 }
 
 func sum3(values ...int) int {
+	var (
+		s int
+		n = len(values)
+	)
+
+	for i := 0; i < n; i++ {
+		s += values[i]
+	}
+
+	return s
+}
+
+func sum4(values ...int) int {
+	var s int
+	for i := range values {
+		s += values[i]
+	}
+
+	return s
+}
+
+func sum5(values ...int) int {
 	var s int
 	for _, v := range values {
 		s += v
 	}
+
 	return s
 }
 
-func sum4(values []int) int {
-	return sumr0(0, len(values), values)
+// sum6 adds values.
+func sum6(values []int) int {
+	return sumr6(0, len(values), values)
 }
 
-func sumr0(i, n int, values []int) int {
-	if i < n {
-		return values[i] + sumr0(i+1, n, values)
+// sumr6 recursively adds values over the range [a,b).
+func sumr6(a, b int, values []int) int {
+	if a < b {
+		return values[a] + sumr6(a+1, b, values)
 	}
+
 	return 0
 }
 
-func sum5(values ...int) int {
-	return sumr1(0, len(values), values...)
+// sum7 adds values.
+func sum7(values ...int) int {
+	return sumr7(0, len(values), values...)
 }
 
-func sumr1(i, n int, values ...int) int {
-	if i < n {
-		return values[i] + sumr1(i+1, n, values...)
+// sumr7 recursively adds values over the range [a,b).
+func sumr7(a, b int, values ...int) int {
+	if a < b {
+		return values[a] + sumr7(a+1, b, values...)
 	}
+
 	return 0
 }
--- sum_test.go
+++ sum_test.go
@@ -1,28 +1,34 @@
 package scratch
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-// newTestInts returns [1, 2, ..., n]
-func newTestInts(n int) []int {
+// intsModN returns [0, 1, ..., n-1]
+func intsModN(n int) []int {
 	ints := make([]int, 0, n)
-	for i := 1; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		ints = append(ints, i)
 	}
+
 	return ints
 }
 
 func TestSums(t *testing.T) {
-	for n := 1; n <= 5; n++ {
+	for n := 0; n < 8; n++ {
 		var (
-			values = newTestInts(n)
-			exp    = (n*n + n) / 2
+			values = intsModN(n)
+			exp    = (n*n - n) / 2 // 1 + 2 + ... + (n-1) = (n^2 - n)/2
 			rec    = map[string]int{
 				"sum0": sum0(values),
 				"sum1": sum1(values),
-				"sum2": sum2(values...),
+				"sum2": sum2(values),
 				"sum3": sum3(values...),
-				"sum4": sum4(values),
+				"sum4": sum4(values...),
 				"sum5": sum5(values...),
+				"sum6": sum6(values),
+				"sum7": sum7(values...),
 			}
 		)
 
@@ -35,19 +41,78 @@ func TestSums(t *testing.T) {
 }
 
 func BenchmarkSums(b *testing.B) {
-	values := newTestInts(256)
-	benchmarkSum(b, values)
-	benchmarkSum0(b, values)
-	benchmarkSum1(b, values)
-	benchmarkSum2(b, values)
-	benchmarkSum3(b, values)
-	benchmarkSum4(b, values)
-	benchmarkSum5(b, values)
+	// Linear scale
+	maxLinear := 8
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumSlice(b, "sum0", sum0, intsModN(n))
+	}
+
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumSlice(b, "sum1", sum1, intsModN(n))
+	}
+
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumSlice(b, "sum2", sum2, intsModN(n))
+	}
+
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumVariadic(b, "sum3", sum3, intsModN(n))
+	}
+
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumVariadic(b, "sum4", sum4, intsModN(n))
+	}
+
+	for n := 0; n < maxLinear; n++ {
+		benchmarkSumVariadic(b, "sum5", sum5, intsModN(n))
+	}
+
+	for n := 0; n < 8; n++ {
+		benchmarkSumSlice(b, "sum6", sum6, intsModN(n))
+	}
+
+	for n := 0; n < 8; n++ {
+		benchmarkSumVariadic(b, "sum7", sum7, intsModN(n))
+	}
+
+	// Exponential scale
+	maxExponential := 256
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumSlice(b, "sum0", sum0, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumSlice(b, "sum1", sum1, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumSlice(b, "sum2", sum2, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumVariadic(b, "sum3", sum3, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumVariadic(b, "sum4", sum4, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumVariadic(b, "sum5", sum5, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumSlice(b, "sum6", sum6, intsModN(n))
+	}
+
+	for n := 1; n <= maxExponential; n <<= 1 {
+		benchmarkSumVariadic(b, "sum7", sum7, intsModN(n))
+	}
 }
 
-func benchmarkSum(b *testing.B, values []int) {
+func benchmarkSumSlice(b *testing.B, fnName string, sum func(values []int) int, values []int) {
 	b.Run(
-		"sum0",
+		fnName+", len = "+strconv.Itoa(len(values)),
 		func(b1 *testing.B) {
 			for j := 0; j < b1.N; j++ {
 				_ = sum(values)
@@ -56,67 +121,12 @@ func benchmarkSum(b *testing.B, values []int) {
 	)
 }
 
-func benchmarkSum0(b *testing.B, values []int) {
+func benchmarkSumVariadic(b *testing.B, fnName string, sum func(values ...int) int, values []int) {
 	b.Run(
-		"sum0",
+		fnName+", len = "+strconv.Itoa(len(values)),
 		func(b1 *testing.B) {
 			for j := 0; j < b1.N; j++ {
-				_ = sum0(values)
-			}
-		},
-	)
-}
-
-func benchmarkSum1(b *testing.B, values []int) {
-	b.Run(
-		"sum1",
-		func(b1 *testing.B) {
-			for j := 0; j < b1.N; j++ {
-				_ = sum1(values)
-			}
-		},
-	)
-}
-
-func benchmarkSum2(b *testing.B, values []int) {
-	b.Run(
-		"sum2",
-		func(b1 *testing.B) {
-			for j := 0; j < b1.N; j++ {
-				_ = sum2(values...)
-			}
-		},
-	)
-}
-
-func benchmarkSum3(b *testing.B, values []int) {
-	b.Run(
-		"sum3",
-		func(b1 *testing.B) {
-			for j := 0; j < b1.N; j++ {
-				_ = sum3(values...)
-			}
-		},
-	)
-}
-
-func benchmarkSum4(b *testing.B, values []int) {
-	b.Run(
-		"sum4",
-		func(b1 *testing.B) {
-			for j := 0; j < b1.N; j++ {
-				_ = sum4(values)
-			}
-		},
-	)
-}
-
-func benchmarkSum5(b *testing.B, values []int) {
-	b.Run(
-		"sum5",
-		func(b1 *testing.B) {
-			for j := 0; j < b1.N; j++ {
-				_ = sum5(values...)
+				_ = sum(values...)
 			}
 		},
 	)
